0d8b9c7e40323ac26df27687345a66b083f18618
Ben Hanna <ben.hanna@vpdcs.com>
2019-10-22 16:49:54 -0400

    Implemented a basic animation.

--- render.c
+++ render.c
@@ -13,12 +13,12 @@
 #define FONT_W 9
 #define FONT_H 16
 
-#define TICKS_PER_SECOND 10
+#define TICKS_PER_SECOND 20
 #define SKIP_TICKS (1000 / TICKS_PER_SECOND)
 
 SDL_Window *window;
 SDL_Renderer *renderer;
-SDL_Texture *texture;
+SDL_Texture *textures[3];
 TTF_Font *font;
 
 void initw() {
@@ -34,9 +34,10 @@ void clearw() {
   SDL_RenderClear(renderer);
 }
 
-void printw(int x, int y, char *str) {
-  SDL_Rect dest = { x * FONT_W, y * FONT_H, FONT_W, FONT_H };
-  SDL_RenderCopy(renderer, texture, NULL, &dest);
+void printw(int x, int y, char i) {
+  SDL_Rect src = { 0, 0, FONT_W * 2, FONT_H };
+  SDL_Rect dest = { x * FONT_W, y * FONT_H, FONT_W * 2, FONT_H };
+  SDL_RenderCopy(renderer, textures[i], &src, &dest);
 }
 
 void refreshw() {
@@ -54,23 +55,30 @@ int main (int argc, char **argv) {
   clearw();
   refreshw();
 
-  Uint32 next_tick, sleep_ticks;
+  Uint32 next_tick = 0;
+  int sleep_ticks = 0;
 
+  char frames[3][2] = {"O ", "Oo", "oO"};
+		     
   SDL_Color color = CYAN;
-  SDL_Surface *text_surface = TTF_RenderText_Solid(font, "\xb0", color);
-  texture = SDL_CreateTextureFromSurface(renderer, text_surface);
-  SDL_FreeSurface(text_surface);
+  for (int i = 0; i < 3; i++) {
+    SDL_Surface *text_surface = TTF_RenderText_Solid(font, frames[i], color);
+    textures[i] = SDL_CreateTextureFromSurface(renderer, text_surface);
+    SDL_FreeSurface(text_surface);
+  }
   
   for (int j = 0; j < 70; j++) {
-    next_tick = SDL_GetTicks() + SKIP_TICKS;
+    for (int a = 0; a < 3; a++) {
+      next_tick = SDL_GetTicks() + SKIP_TICKS;
     
-    clearw();
-    printw(j, 15, "");
-    refreshw();
+      clearw();
+      printw(j, 15, a);
+      refreshw();
 
-    sleep_ticks = next_tick - SDL_GetTicks();
-    if (sleep_ticks >= 0) {
-      SDL_Delay(sleep_ticks);
+      sleep_ticks = next_tick - SDL_GetTicks();
+      if (sleep_ticks > 0) {
+        SDL_Delay(sleep_ticks);
+      }
     }
   }
   
